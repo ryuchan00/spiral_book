feab0a9b0aa03afa8e9f05d68651e7e3b270fc85
ryuchun00 <leokun0210@gmail.com>
2019-09-25 01:34:43 +0900

    stableなソートができた

--- stable_sort.rb
+++ stable_sort.rb
@@ -5,7 +5,7 @@ def isStable(n:, input:, out:)
     (i + 1).upto(n) do |j|
       0.upto(n) do |a|
         (a + 1).upto(n) do |b|
-          if input[i] == input[j] && input[i] == out[b] && input[j] == out[a]
+          if input[i][1] == input[j][1] && input[i] == out[b] && input[j] == out[a]
             return false
           end
         end
@@ -17,9 +17,8 @@ end
 
 n = gets.to_i
 c = gets.chomp.split.map(&:to_s)
-bubble_c = c.clone
-
 # bubble sort
+bubble_c = c.clone
 0.upto(n -1) do |i|
   (n - 1).downto(i + 1) do |j|
     if bubble_c[j][1].to_i < bubble_c[j -1][1].to_i
@@ -31,3 +30,19 @@ bubble_c = c.clone
 end
 puts bubble_c.join(' ')
 puts isStable(n: n, input: c, out: bubble_c) ? 'Stable' : 'Not stable'
+
+# selection sort
+select_c = c.clone
+0.upto(n -1) do |i|
+  minj = i
+  i.upto(n - 1) do |j|
+    if select_c[j][1] < select_c[minj][1]
+      minj = j
+    end
+  end
+  tmp = select_c[i]
+  select_c[i] = select_c[minj]
+  select_c[minj] = tmp
+end
+puts select_c.join(' ')
+puts isStable(n: n, input: c, out: select_c) ? 'Stable' : 'Not stable'
